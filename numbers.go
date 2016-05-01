1da8f568a34c0f20bf149113dea7a6db251cdbc6
José Luis Quintana <joseluisquintana20@gmail.com>
2016-04-30 22:28:49 -0500

    Number operations

---
+++ numbers.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("1000 + 2 = ", 1000 + 2)
+  fmt.Println("1000 - 2 = ", 1000 - 2)
+  fmt.Println("1000 * 2 = ", 1000 * 2)
+  fmt.Println("1000 / 2 = ", 1000 / 2)
+  fmt.Println("1000 % 2 = ", 1000 % 2)
+}
+

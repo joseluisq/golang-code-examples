997b11e56d2907c497bf4cfb8cd2cd3ee56eb92a
José Luis Quintana <joseluisquintana20@gmail.com>
2016-05-02 14:23:16 -0500

    For and If statements

---
+++ if.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+// Show odd or even values
+func main() {
+  for i := 1; i <= 10; i++ {
+    if i % 2 == 0 {
+      fmt.Println(i, "is even")
+    } else {
+      fmt.Println(i, "is odd")
+    }
+  }
+}
+
